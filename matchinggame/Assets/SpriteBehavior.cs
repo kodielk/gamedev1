a6c2d9f02b665da0eb3dd813b351cdf6c02a235a
Elise Cobabe <10920472@uvu.edu>
2024-09-09 23:26:02 -0600

    Matching game

---
+++ matchinggame/Assets/SpriteBehavior.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteBehavior : MonoBehaviour
+{
+    private SpriteRenderer rendererObj;
+
+    private void Start()
+    {
+        rendererObj = GetComponent<SpriteRenderer>();
+    }
+
+    public void ChangeRendererColor(ColorID obj)
+    {
+        rendererObj.color = obj.value;
+    }
+}
