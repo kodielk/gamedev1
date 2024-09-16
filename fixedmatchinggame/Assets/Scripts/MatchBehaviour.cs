5f8c93ae7fab4b074ab2d5d8fcb28b1176ca9129
Elise Cobabe <10920472@uvu.edu>
2024-09-15 23:23:52 -0600

    FINALly fixed the matching game. Added audio, Fixed code, the whole enchilada

---
+++ fixedmatchinggame/Assets/Scripts/MatchBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+public class MatchBehaviour : MonoBehaviour
+
+{
+    public ID idObj;
+    public UnityEvent matchEvent, noMatchEvent, nomMatchDelayedEvent;
+    private IEnumerator OnTriggerEnter(Collider other)
+    {
+        var tempObj = other.GetComponent<IDContainerBehaviour>();
+        if (tempObj == null)
+            yield break;
+        
+        var otherID = tempObj.idObj;
+        if (otherID == idObj)
+        {
+            matchEvent.Invoke();
+        }
+        else
+        {
+            noMatchEvent.Invoke();
+            yield return new WaitForSeconds(0.5f);
+            nomMatchDelayedEvent.Invoke();
+        }
+    }
+}
