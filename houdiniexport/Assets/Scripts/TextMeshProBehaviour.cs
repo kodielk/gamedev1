b9f50d81d02906a353dc0f70b2365d51cb17a765
Elise Cobabe <10920472@uvu.edu>
2024-10-04 19:42:13 -0600

    Added capsule and platforms,

---
+++ houdiniexport/Assets/Scripts/TextMeshProBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+public class TextMeshProBehaviour : MonoBehaviour
+{
+    private TextMeshProUGUI label;
+    public UnityEvent startEvent;
+
+    private void Start()
+    {
+        label = GetComponent<TextMeshProUGUI>();
+        startEvent.Invoke();
+    }
+
+    public void UpdateLabel(FloatData obj)
+    {
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    public void UpdateLabel(IntData obj)
+    {
+        label.text = obj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
